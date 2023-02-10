18527609ce1a2ccc0416506fe4c4c34e8d16ce32
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-02-10 09:44:17 +0100

    Add validation to posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,8 @@ class Post < ApplicationRecord
   def recent_5_comments
     comments.limit(5).order(created_at: :desc)
   end
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
