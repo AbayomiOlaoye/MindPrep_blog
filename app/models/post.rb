89f043feaecf555233846088e9fe879c3d4758a2
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-02-14 14:46:14 +0100

    Add user and Post controllers

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,18 +4,18 @@ class Post < ApplicationRecord
   has_many :likes
   after_save :update_post_counter
 
-  private
-
-  def update_posts_counter
-    author.increment!(:posts_counter)
-  end
+  validates :title, :text, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_5_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  validates :title, :text, presence: true
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
