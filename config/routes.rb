914528158b118b2a070bb65e4dca829a8feb3179
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-02-17 11:00:01 +0100

    Refactor code to feature only required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'posts/index'
-  get 'posts/show'
+  root to: 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :post, only: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
