2e228fd0369f7edfba833f9486ba464496e87df1
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-02-17 16:51:48 +0100

    Render Comment views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'likes/create'
-  get 'likes/like_params'
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/comment_params'
-  root to: 'users#index'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  get 'users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users', to: 'users#index', as: 'users'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: %i[create]
+    end
+  end
 end
