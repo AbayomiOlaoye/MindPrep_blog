03ed669298a09f276d24c29eff608917bf00e77f
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-02-17 16:36:50 +0100

    Update ApplicationRecords and create form for data

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,7 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   after_save :update_posts_likes_counter
+  validates :author_id, uniqueness: { scope: :post_id }
 
   private
 
