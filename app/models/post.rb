7845725ad0fea35957f3a1ec76847f0ed67fc1c5
Oluwatoyin <mindprepopps@gmail.com>
2023-02-25 01:04:53 +0100

    Add more validation to model/post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   validates :title, :text, presence: true
+  validates :title, presence: true,
+                    length: { maximum: 250, too_long: 'Title must not exceed 250 characters' }
+  validates_numericality_of :comments_counter, only_integer: true, greater_than_or_equal: 0
+  validates_numericality_of :likes_counter, only_integer: true, greater_than_or_equal: 0
 
   def recent_5_comments
     comments.order(created_at: :desc).limit(5)
