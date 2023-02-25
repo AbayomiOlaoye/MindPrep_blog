5ef1f23a946d10e5c393ba1d6f64cc6527dade8d
Olaoye Oluwatoyin Abayomi <mindprepopps@gmail.com>
2023-02-25 17:58:52 +0100

    Remove unnecessary comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   # create a new user
   before(:each) do
     @user = User.new(name: 'Test User', bio: 'Test Bio', photo: 'Test Photo')
